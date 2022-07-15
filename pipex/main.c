093e58a354eb15c3694705a3fc73a711510aee04
Santiago Ocana barrera <socana-b@c1r12s5.42madrid.com>
2022-07-15 18:43:52 +0200

    Empezando el pipex

--- pipex/main.c
+++ pipex/main.c
@@ -1,10 +1,14 @@
 #include "pipex.h"
 
-int main(int argc, char **argv){
-	if (argc < 5)
+int main(int argc, char **argv)
+{
+
+
+	if (argc == 5)
 	{
-		write(1, "Error: El numero de argumentos no es valido\n", 44);
-		return (0);
+		
 	}
+	else
+		write(1, "Error: El numero de argumentos no es valido\n", 44);
 	return (0);
 }
